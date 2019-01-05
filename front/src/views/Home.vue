5cf3ae7412bfe8e84b252f5dd25b59d494c6025c
kreciszd <kreciszd@gmail.com>
2019-01-05 18:55:27 +0100

    Front init

---
+++ front/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        a: 1
+      }
+    },
+    methods: {
+
+      getText() {
+
+        return'dsad';
+      }
+    },
+  }
+</script>
